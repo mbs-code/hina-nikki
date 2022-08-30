474181687c1c2c8d1244ff91006a91e7671737fe
mbs-code <mbscodec@gmail.com>
2022-08-30 23:29:40 +0900

    fix: hashtag を16文字制限に修正

--- src/utils/RegexUtil.ts
+++ src/utils/RegexUtil.ts
@@ -1,6 +1,6 @@
 export class RegexUtil {
   public static diaryRegex = /^\d{4}-\d{2}-\d{2}$/ //  ISO 8601 date: yyyy-mm-dd
-  public static hashRegex = /^#[^#\s\v].+$/ // 垂直タブだけ除外されるらしい
+  public static hashtagRegex = /^#[^#\s\v].{1,15}$/ // 垂直タブだけ除外されるらしい, 2+15文字まで
   // eslint-disable-next-line no-irregular-whitespace
   public static separateRegex = /\s|\v|\r\n|\n/ // スペースと改行に反応
 
@@ -9,7 +9,7 @@ export class RegexUtil {
   }
 
   public static isHashtagTitle (val?: string): boolean {
-    return val ? this.hashRegex.test(val) : false
+    return val ? this.hashtagRegex.test(val) : false
   }
 
   public static hasSeparate (val?: string): boolean {

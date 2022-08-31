9881f8ea2a82059fb878261b219b824f805effd9
mbs-code <mbscodec@gmail.com>
2022-08-31 20:06:31 +0900

    add: dev 時に自動的にdebugとする

--- src/plugins/migrate.ts
+++ src/plugins/migrate.ts
@@ -2,7 +2,7 @@ import { Database } from '~~/src/databases/Database'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('app:created', async () => {
-    // Database.debug = true
+    Database.debug = process?.dev
 
     // TODO: 実行時にバックアップと、ダイアログ表示？
     const { migrator } = Database.getInstance()
